56e702750af6964cc14ac6d8f181003ec645b5de
Chris Bell <chbell@practicevelocity.com>
2019-06-13 08:51:51 -0500

    Added readme to top nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ReadMeComponent } from './read-me/read-me.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'read-me', component: ReadMeComponent }
 ];
 
 @NgModule({
