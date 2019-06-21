00c04eec16887ad5d4414d5ae7d3dcec2c0b841c
Chris Bell <chbell@practicevelocity.com>
2019-06-21 09:58:04 -0500

    index fiddling and semi-working nav changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { HomeComponent } from './home/home.component';
 import { ReadMeComponent } from './read-me/read-me.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   { path: 'read-me', component: ReadMeComponent },
   {
     path: 'ui-examples',
